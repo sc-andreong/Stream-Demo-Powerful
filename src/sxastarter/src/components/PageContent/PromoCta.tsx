0b227aa1dfa233deff953a2c1bd470f14fea18ce
Stanislav Stativa <stas.stativa@gmail.com>
2025-01-10 00:59:34 +0200

    fix Promo Cta

--- src/sxastarter/src/components/PageContent/PromoCta.tsx
+++ src/sxastarter/src/components/PageContent/PromoCta.tsx
@@ -15,6 +15,7 @@ import { ParallaxBackgroundImage } from 'components/NonSitecore/ParallaxBackgrou
 import useVisibility from 'src/hooks/useVisibility';
 import { ComponentProps } from 'lib/component-props';
 import { DottedAccent } from 'components/NonSitecore/DottedAccent';
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 interface Fields {
   Eyebrow: Field<string>;
@@ -39,148 +40,35 @@ export const Default = (props: PromoCtaProps): JSX.Element => {
   const [isVisible, domRef] = useVisibility();
 
   return (
-    <div
-      className={`component promo-cta ${props.params.styles.trimEnd()}`}
-      id={id ? id : undefined}
-      ref={domRef}
-    >
-      <div className="container">
-        <div className="row row-gap-4 main-content align-items-center">
-          <div className="col-lg-5 text-center text-lg-start">
-            <h6 className="eyebrow-accent">
-              <Text field={props.fields.Eyebrow} />
-            </h6>
-            <h1 className="display-6 fw-bold mb-3">
-              <Text field={props.fields.Title} />
-            </h1>
-            <div className="promo-cta-text">
-              <p className="fs-5">
-                <Text field={props.fields.Subtitle} />
-              </p>
+    <ParallaxProvider>
+      <div
+        className={`component promo-cta ${props?.params?.styles?.trimEnd()}`}
+        id={id ? id : undefined}
+        ref={domRef}
+      >
+        <div className="container">
+          <div className="row row-gap-4 main-content align-items-center">
+            <div className="col-lg-5 text-center text-lg-start">
+              <h6 className="eyebrow-accent">
+                <Text field={props.fields.Eyebrow} />
+              </h6>
+              <h1 className="display-6 fw-bold mb-3">
+                <Text field={props.fields.Title} />
+              </h1>
+              <div className="promo-cta-text">
+                <p className="fs-5">
+                  <Text field={props.fields.Subtitle} />
+                </p>
 
-              <RichText field={props.fields.Text} className="text-content" />
+                <RichText field={props.fields.Text} className="text-content" />
 
-              {(isPageEditing || props.fields?.Link?.value?.href) && (
-                <Link field={props.fields.Link} className="button button-main mt-3 me-4" />
-              )}
-            </div>
-          </div>
-          <div className="col-md-10 mx-auto col-lg-7 mx-lg-0">
-            <div className="image-wrapper">
-              <DottedAccent className="dotted-accent-top" />
-              <Image
-                field={props.fields.Image}
-                className={`d-block mx-lg-auto img-fluid ${
-                  !isPageEditing ? `fade-section ${isVisible ? 'is-visible' : ''}` : ''
-                }`}
-              ></Image>
-              <DottedAccent className="dotted-accent-bottom" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export const Kiosk = (props: PromoCtaProps): JSX.Element => {
-  const id = props.params.RenderingIdentifier;
-  const { sitecoreContext } = useSitecoreContext();
-  const isPageEditing = sitecoreContext.pageEditing;
-  const [isVisible, domRef] = useVisibility();
-
-  return (
-    <div
-      className={`component promo-cta kiosk ${props.params.styles.trimEnd()}`}
-      id={id ? id : undefined}
-      ref={domRef}
-    >
-      <div className="container-fluid">
-        <div className="row row-gap-4">
-          <div className="col-lg-5 text-center text-lg-start p-5">
-            <h6 className="eyebrow-accent">
-              <Text field={props.fields.Eyebrow} />
-            </h6>
-            <h1 className="display-2 fw-bold mb-3">
-              <Text field={props.fields.Title} />
-            </h1>
-            <div className="promo-cta-text">
-              <RichText field={props.fields.Text} className="text-content" />
-
-              {(isPageEditing || props.fields?.Link?.value?.href) && (
-                <Link field={props.fields.Link} className="button button-main mt-3 me-4" />
-              )}
-            </div>
-          </div>
-          <div className="d-flex align-items-top mx-auto col-lg-5 mx-lg-0 p-5">
-            <div className="image-wrapper">
-              <DottedAccent className="dotted-accent-top" />
-              <Image
-                field={props.fields.Image}
-                className={`d-block mx-lg-auto img-fluid ${
-                  !isPageEditing ? `fade-section ${isVisible ? 'is-visible' : ''}` : ''
-                }`}
-              ></Image>
-              <DottedAccent className="dotted-accent-bottom" />
-            </div>
-          </div>
-          <div className="d-flex align-items-end mx-auto col-lg-2">
-            <Image field={props.fields.Image2}></Image>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export const WithPlaceholderColumn = (props: PromoCtaProps): JSX.Element => {
-  const id = props.params.RenderingIdentifier;
-  const { sitecoreContext } = useSitecoreContext();
-  const isPageEditing = sitecoreContext.pageEditing;
-  const [isVisible, domRef] = useVisibility();
-
-  return (
-    <div
-      className={`component promo-cta with-placeholder-column ${props.params.styles.trimEnd()}`}
-      id={id ? id : undefined}
-      ref={domRef}
-    >
-      <div className="container">
-        <div className="row row-gap-4 main-content align-items-center">
-          <div className="col-lg-5 text-center text-lg-start">
-            <h6 className="eyebrow-accent">
-              <Text field={props.fields.Eyebrow} />
-            </h6>
-            <h1 className="fs-1 fw-bold mb-3">
-              <Text field={props.fields.Title} />
-            </h1>
-            <div className="promo-cta-text">
-              <p className="fs-5">
-                <Text field={props.fields.Subtitle} />
-              </p>
-
-              <RichText field={props.fields.Text} className="text-content" />
-
-              {(isPageEditing || props.fields?.Link?.value?.href) && (
-                <Link field={props.fields.Link} className="button button-main mt-3" />
-              )}
-              {(isPageEditing || props.fields?.Link2?.value?.href) && (
-                <Link field={props.fields.Link2} className="button button-simple mt-3 mx-4" />
-              )}
-            </div>
-          </div>
-
-          <div className="col-md-12 mx-auto col-lg-7 mx-lg-0">
-            <div className="row align-items-center">
-              <div className="promo-cta-placeholder col-12 col-md-9">
-                <div className="promo-cta-placeholder-inner">
-                  <div className="row">
-                    <Placeholder name="promo-cta" rendering={props.rendering} />
-                  </div>
-                </div>
+                {(isPageEditing || props.fields?.Link?.value?.href) && (
+                  <Link field={props.fields.Link} className="button button-main mt-3 me-4" />
+                )}
               </div>
-
-              <div className="image-wrapper d-none d-md-block col-md-8">
+            </div>
+            <div className="col-md-10 mx-auto col-lg-7 mx-lg-0">
+              <div className="image-wrapper">
                 <DottedAccent className="dotted-accent-top" />
                 <Image
                   field={props.fields.Image}
@@ -194,7 +82,126 @@ export const WithPlaceholderColumn = (props: PromoCtaProps): JSX.Element => {
           </div>
         </div>
       </div>
-    </div>
+    </ParallaxProvider>
+  );
+};
+
+export const Kiosk = (props: PromoCtaProps): JSX.Element => {
+  const id = props.params.RenderingIdentifier;
+  const { sitecoreContext } = useSitecoreContext();
+  const isPageEditing = sitecoreContext.pageEditing;
+  const [isVisible, domRef] = useVisibility();
+
+  return (
+    <ParallaxProvider>
+      <div
+        className={`component promo-cta kiosk ${props?.params?.styles?.trimEnd()}`}
+        id={id ? id : undefined}
+        ref={domRef}
+      >
+        <div className="container-fluid">
+          <div className="row row-gap-4">
+            <div className="col-lg-5 text-center text-lg-start p-5">
+              <h6 className="eyebrow-accent">
+                <Text field={props.fields.Eyebrow} />
+              </h6>
+              <h1 className="display-2 fw-bold mb-3">
+                <Text field={props.fields.Title} />
+              </h1>
+              <div className="promo-cta-text">
+                <RichText field={props.fields.Text} className="text-content" />
+
+                {(isPageEditing || props.fields?.Link?.value?.href) && (
+                  <Link field={props.fields.Link} className="button button-main mt-3 me-4" />
+                )}
+              </div>
+            </div>
+            <div className="d-flex align-items-top mx-auto col-lg-5 mx-lg-0 p-5">
+              <div className="image-wrapper">
+                <DottedAccent className="dotted-accent-top" />
+                <Image
+                  field={props.fields.Image}
+                  className={`d-block mx-lg-auto img-fluid ${
+                    !isPageEditing ? `fade-section ${isVisible ? 'is-visible' : ''}` : ''
+                  }`}
+                ></Image>
+                <DottedAccent className="dotted-accent-bottom" />
+              </div>
+            </div>
+            <div className="d-flex align-items-end mx-auto col-lg-2">
+              <Image field={props.fields.Image2}></Image>
+            </div>
+          </div>
+        </div>
+      </div>
+    </ParallaxProvider>
+  );
+};
+
+export const WithPlaceholderColumn = (props: PromoCtaProps): JSX.Element => {
+  const id = props.params.RenderingIdentifier;
+  const { sitecoreContext } = useSitecoreContext();
+  const isPageEditing = sitecoreContext.pageEditing;
+  const [isVisible, domRef] = useVisibility();
+
+  return (
+    <ParallaxProvider>
+      <div
+        className={`component promo-cta with-placeholder-column ${props?.params?.styles?.trimEnd()}`}
+        id={id ? id : undefined}
+        ref={domRef}
+      >
+        <div className="container">
+          <div className="row row-gap-4 main-content align-items-center">
+            <div className="col-lg-5 text-center text-lg-start">
+              <h6 className="eyebrow-accent">
+                <Text field={props.fields.Eyebrow} />
+              </h6>
+              <h1 className="fs-1 fw-bold mb-3">
+                <Text field={props.fields.Title} />
+              </h1>
+              <div className="promo-cta-text">
+                <p className="fs-5">
+                  <Text field={props.fields.Subtitle} />
+                </p>
+
+                <RichText field={props.fields.Text} className="text-content" />
+
+                {(isPageEditing || props.fields?.Link?.value?.href) && (
+                  <Link field={props.fields.Link} className="button button-main mt-3" />
+                )}
+                {(isPageEditing || props.fields?.Link2?.value?.href) && (
+                  <Link field={props.fields.Link2} className="button button-simple mt-3 mx-4" />
+                )}
+              </div>
+            </div>
+
+            <div className="col-md-12 mx-auto col-lg-7 mx-lg-0">
+              <div className="row align-items-center">
+                <div className="promo-cta-placeholder col-12 col-md-9">
+                  <div className="promo-cta-placeholder-inner">
+                    <div className="row">
+                      <Placeholder name="promo-cta" rendering={props.rendering} />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="image-wrapper d-none d-md-block col-md-8">
+                  <DottedAccent className="dotted-accent-top" />
+                  <Image
+                    field={props.fields.Image}
+                    className={`d-block mx-lg-auto img-fluid ${
+                      !isPageEditing ? `fade-section ${isVisible ? 'is-visible' : ''}` : ''
+                    }`}
+                  ></Image>
+                  <DottedAccent className="dotted-accent-bottom" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </ParallaxProvider>
   );
 };
 
@@ -204,27 +211,29 @@ export const WithBackgroundImage = (props: PromoCtaProps): JSX.Element => {
   const isPageEditing = sitecoreContext.pageEditing;
 
   return (
-    <div
-      className={`component promo-cta with-background-image ${props.params.styles.trimEnd()}`}
-      id={id ? id : undefined}
-    >
-      <ParallaxBackgroundImage BackgroundImage={props.fields.Image} />
-      <div className="container">
-        <div className="row justify-content-center main-content">
-          <div className="col-12 mx-auto">
-            <h1 className="display-3 fw-bold text-center mb-3">
-              <Text field={props.fields.Title} />
-            </h1>
-            <div className="fs-3 text-center">
-              <RichText field={props.fields.Text} />
+    <ParallaxProvider>
+      <div
+        className={`component promo-cta with-background-image ${props?.params?.styles?.trimEnd()}`}
+        id={id ? id : undefined}
+      >
+        <ParallaxBackgroundImage BackgroundImage={props.fields.Image} />
+        <div className="container">
+          <div className="row justify-content-center main-content">
+            <div className="col-12 mx-auto">
+              <h1 className="display-3 fw-bold text-center mb-3">
+                <Text field={props.fields.Title} />
+              </h1>
+              <div className="fs-3 text-center">
+                <RichText field={props.fields.Text} />
 
-              {(isPageEditing || props.fields?.Link?.value?.href) && (
-                <Link field={props.fields.Link} className="button button-main mt-3" />
-              )}
+                {(isPageEditing || props.fields?.Link?.value?.href) && (
+                  <Link field={props.fields.Link} className="button button-main mt-3" />
+                )}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </ParallaxProvider>
   );
 };
