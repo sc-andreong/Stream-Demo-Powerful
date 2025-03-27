ff20231e87ff3e97f9f045bf015aaace57ac087c
Andre Ong <andreong28@gmail.com>
2025-03-27 15:48:55 +0800

    add drop components to qa andre 2

--- src/sxastarter/src/components/PageContent/QA_Andre_2.tsx
+++ src/sxastarter/src/components/PageContent/QA_Andre_2.tsx
@@ -12,6 +12,12 @@ interface Fields {
   Integer2: Field<string>;
   Number1: Field<string>;
   Number2: Field<string>;
+  DropList1: Field<string>;
+  DropList2: Field<string>;
+  DropLink1: Field<string>;
+  DropLink2: Field<string>;
+  DropTree1: Field<string>;
+  DropTree2: Field<string>;
 }
 
 export type QAProps = {
@@ -34,36 +40,85 @@ export const Default = (props: QAProps): JSX.Element => {
       className={`component qa-andre-1 ${props?.params?.styles?.trimEnd()}`}
       id={id ? id : undefined}
     >
-      <div className="subtitle">
-        <Text field={props.fields?.SingleLine1} />
+      <div className='container'>
+        <p className='title'>Single Line</p>
+        <div className="subtitle">
+          <Text field={props.fields?.SingleLine1} />
+        </div>
+        <p className="subtitle">
+          <Text field={props.fields?.SingleLine2} />
+        </p>
       </div>
-      <p className="subtitle">
-        <Text field={props.fields?.SingleLine2} />
-      </p>
-      <p className="subtitle">
-        <Text field={props.fields?.MultiLine1} />
-      </p>
-      <p className="subtitle">
-        <Text field={props.fields?.MultiLine2} />
-      </p>
-      <p className="subtitle">
-        <RichText field={props.fields?.RichText1} />
-      </p>
-      <p className="subtitle">
-        <RichText field={props.fields?.RichText2} />
-      </p>
-      <p className="subtitle">
-        <Text field={props.fields?.Integer1} />
-      </p>
-      <p className="subtitle">
-        <Text field={props.fields?.Integer2} />
-      </p>
-      <p className="subtitle">
-        <Text field={props.fields?.Number1} />
-      </p>
-      <p className="subtitle">
-        <Text field={props.fields?.Number2} />
-      </p>
+
+      <div className='container'>
+        <p className='title'>Multi Line</p>
+        <p className="subtitle">
+          <Text field={props.fields?.MultiLine1} />
+        </p>
+        <p className="subtitle">
+          <Text field={props.fields?.MultiLine2} />
+        </p>
+      </div>
+
+      <div className='container'>
+        <p className='title'>Rich Text</p>
+        <p className="subtitle">
+          <RichText field={props.fields?.RichText1} />
+        </p>
+        <p className="subtitle">
+          <RichText field={props.fields?.RichText2} />
+        </p>
+      </div>
+
+      <div className='container'>
+        <p className='title'>Integer</p>
+        <p className="subtitle">
+          <Text field={props.fields?.Integer1} />
+        </p>
+        <p className="subtitle">
+          <Text field={props.fields?.Integer2} />
+        </p>
+      </div>
+
+      <div className='container'>
+        <p className='title'>Number</p>
+        <p className="subtitle">
+          <Text field={props.fields?.Number1} />
+        </p>
+        <p className="subtitle">
+          <Text field={props.fields?.Number2} />
+        </p>
+      </div>
+
+      <div className='container'>
+        <p className='title'>Drop List</p>
+        <p className="subtitle">
+          <Text field={props.fields?.DropList1} />
+        </p>
+        <p className="subtitle">
+          <Text field={props.fields?.DropList1} />
+        </p>
+      </div>
+
+      <div className='container'>
+        <p className='title'>Drop Link</p>
+        <p className="subtitle">
+          <Text field={props.fields?.DropLink1} />
+        </p>
+        <p className="subtitle">
+          <Text field={props.fields?.DropLink2} />
+        </p>
+      </div>
+
+      <div className='container'>
+        <p className='title'>Drop Tree</p>
+        <p className="subtitle">
+          <Text field={props.fields?.DropTree1} />
+        </p>
+        <p className="subtitle">
+          <Text field={props.fields?.DropTree2} />
+        </p>
+      </div>
     </div>
   ) : (
     <ComponentDefault {...props} />
