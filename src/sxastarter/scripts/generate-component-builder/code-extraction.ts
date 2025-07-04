886fdc4514bcb4ef6fec12ce0ab3db1e371e025a
Stanislav Stativa <stas.stativa@gmail.com>
2025-07-04 22:31:47 +0300

    add code extraction

---
+++ src/sxastarter/scripts/generate-component-builder/code-extraction.ts
@@ -0,0 +1,16 @@
+import { extractFiles } from '@sitecore-jss/sitecore-jss-dev-tools';
+import { ComponentBuilderPlugin, ComponentBuilderPluginConfig } from '.';
+
+/**
+ * If consented to, sends components code for code generation learning to XMC
+ */
+class CodeExtractionPlugin implements ComponentBuilderPlugin {
+  order = 100;
+
+  exec(config: ComponentBuilderPluginConfig) {
+    extractFiles({});
+    return config;
+  }
+}
+
+export const codeExtractionPlugin = new CodeExtractionPlugin();
