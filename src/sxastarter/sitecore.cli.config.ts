16c2b99c4fa65373fc1f18b32c939b711dda36b3
Stanislav Stativa <stas.stativa@gmail.com>
2025-07-07 14:22:52 +0300

    fix

--- src/sxastarter/sitecore.cli.config.ts
+++ src/sxastarter/sitecore.cli.config.ts
@@ -1,5 +1,5 @@
 import config from './sitecore.config';
-import { debug } from '@sitecore-content-sdk/core';
+import { enableDebug } from '@sitecore-content-sdk/core';
 import { defineCliConfig } from '@sitecore-content-sdk/nextjs/config';
 import { generateSites, generateMetadata, extractFiles } from '@sitecore-content-sdk/nextjs/tools';
 
@@ -8,7 +8,7 @@ if (process.env.DEBUG) {
     .map((scope) => scope.trim())
     .filter(Boolean);
   console.log(`Debug enabled for scopes: [${debugScopes.join(', ')}]`);
-  debug.enable(process.env.DEBUG);
+  enableDebug(process.env.DEBUG);
 }
 
 export default defineCliConfig({
