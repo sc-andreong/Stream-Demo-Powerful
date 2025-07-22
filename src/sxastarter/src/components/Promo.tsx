4fa3a4ac8de70baf736c533187654af9fb2a194e
Wei Bin Chiu <weibin.chiu@sitecore.com>
2025-07-22 16:03:40 +0800

    Update empty hint text in PromoDefaultComponent

--- src/sxastarter/src/components/Promo.tsx
+++ src/sxastarter/src/components/Promo.tsx
@@ -16,7 +16,7 @@ type PromoProps = {
 const PromoDefaultComponent = (props: PromoProps): JSX.Element => (
   <div className={`component promo ${props.params.styles}`}>
     <div className="component-content">
-      <span className="is-empty-hint">Promo</span>
+        <span className="is-empty-hint">Promo test</span>
     </div>
   </div>
 );
